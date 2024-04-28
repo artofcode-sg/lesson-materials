9ab0106b859caa33d0f157b926e23e4db9528fda
yamgao_ <liu_yanzhao@outlook.com>
2024-04-28 11:06:53 +0800

    ArtOfCode Python Installer

---
+++ artofcode-installer-win-x86_32.ps1
@@ -0,0 +1,9 @@
+$usrname = (whoami).split("\")[1]
+
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+Invoke-WebRequest -Uri "https://www.python.org/ftp/python/3.12.3/python-3.12.3.exe" -OutFile "C:\Users\$usrname\python-3.12.3.exe"
+cd C:\Users\$usrname\
+.\python-3.12.3.exe /quiet InstallAllUsers=0 InstallLauncherAllUsers=0 PrependPath=1 Include_test=0
+
+py -m pip install notebook
+jupyter notebook
